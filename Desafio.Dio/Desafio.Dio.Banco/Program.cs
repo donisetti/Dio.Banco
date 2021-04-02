e34b0e389bcbb74390daa7f58652ed36782adc27
Donisetti Ferreira Cosma <donisetti@msn.com>
2021-04-01 22:13:39 -0400

    Criação do projeto console

---
+++ Desafio.Dio/Desafio.Dio.Banco/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Desafio.Dio.Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Hello World!");
+
+
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
