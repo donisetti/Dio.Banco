d6cce31f9d7945e13b144d64860f4a812c45c56a
Donisetti Ferreira Cosma <donisetti@msn.com>
2021-04-01 22:44:20 -0400

    Menu Principal

--- Desafio.Dio/Desafio.Dio.Banco/Program.cs
+++ Desafio.Dio/Desafio.Dio.Banco/Program.cs
@@ -10,13 +10,86 @@ namespace Desafio.Dio.Banco
 
         static void Main(string[] args)
         {
-            Console.Clear();
-            Console.WriteLine("Hello World!");
+			string opcaoUsuario = ObterOpcaoUsuario();
 
+			while (opcaoUsuario.ToUpper() != "X")
+			{
+				switch (opcaoUsuario)
+				{
+					case "1":
+						ListarContas();
+						break;
+					case "2":
+						InserirConta();
+						break;
+					case "3":
+						Transferir();
+						break;
+					case "4":
+						Sacar();
+						break;
+					case "5":
+						Depositar();
+						break;
+					case "C":
+						Console.Clear();
+						break;
 
-            Console.ReadLine();
-            Console.Clear();
+					default:
+						throw new ArgumentOutOfRangeException();
+				}
+
+				opcaoUsuario = ObterOpcaoUsuario();
+			}
+
+			Console.WriteLine("Obrigado por utilizar nossos serviços.");
+			Console.ReadLine();
+		}
+
+        private static void Depositar()
+        {
+            throw new NotImplementedException();
         }
+
+        private static void Sacar()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Transferir()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void InserirConta()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ListarContas()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+			Console.WriteLine();
+			Console.WriteLine("DIO Bank a seu dispor!!!");
+			Console.WriteLine("Informe a opção desejada:");
+
+			Console.WriteLine("1- Listar contas");
+			Console.WriteLine("2- Inserir nova conta");
+			Console.WriteLine("3- Transferir");
+			Console.WriteLine("4- Sacar");
+			Console.WriteLine("5- Depositar");
+			Console.WriteLine("C- Limpar Tela");
+			Console.WriteLine("X- Sair");
+			Console.WriteLine();
+
+			string opcaoUsuario = Console.ReadLine().ToUpper();
+			Console.WriteLine();
+			return opcaoUsuario;
+		}
     }
 
    
